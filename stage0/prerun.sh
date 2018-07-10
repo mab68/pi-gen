b8373894533c522ea542368658947f0fd3fbed7c
mab68 <33644913+mab68@users.noreply.github.com>
2018-07-10 14:52:19 +1200

    Update prerun.sh

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -2,4 +2,5 @@
 
 if [ ! -d "${ROOTFS_DIR}" ]; then
 	bootstrap stretch "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
+	cat work/2018-07-10-faucet-raspbian/stage0/debootstrap/debootstrap.log
 fi
